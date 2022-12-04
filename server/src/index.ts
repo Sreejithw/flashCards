0a3311e89ce920553cf0df87f717f35648ca9310
Sreejith Warrier <sreejith_w@outlook.com>
2022-12-04 17:18:08 +1100

    Landing Page + cors erros fix

--- server/src/index.ts
+++ server/src/index.ts
@@ -3,14 +3,23 @@ config();
 
 import express,{ Request, Response } from "express";
 import mongoose from 'mongoose';
-
-
+import cors from 'cors';
 import IndexDeck from './models/IndexDeck';
 
 const PORT = 5000;
 const app = express();
+
+app.use(
+    cors({
+        origin: "*",
+    })
+);
 app.use(express.json());
 
+app.get("/decks", async (req: Request, res: Response) => {
+    const decks = await IndexDeck.find();
+    res.json(decks);
+})
 
 app.post("/decks", async (req: Request ,res: Response) => {
     const newDeck = new IndexDeck({    
